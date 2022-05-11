9b6d16489e80c6d1b9b857681d84f6bf8cd1aaa6
peron17 <tommypriambodo@gmail.com>
2022-05-11 10:25:41 +0700

    book models, factory, controller

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title', 'author', 'synopsys', 'publisher', 'cover', 'released_date', 'price', 'category_id', 'sold'
+    ];
 }
