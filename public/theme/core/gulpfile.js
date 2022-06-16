fd198f97bd96d69091757f8696b600a43c831ddc
peron17 <tommypriambodo@gmail.com>
2022-06-16 13:22:13 +0700

    form login, regis, forgot password template

--- public/theme/core/gulpfile.js
+++ public/theme/core/gulpfile.js
@@ -1,36 +1,35 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass')(require('sass'));
-var uglify = require('gulp-uglifycss');
-var browserSync = require('browser-sync').create();
-var strip = require('gulp-strip-comments');
-const { watch } = require('browser-sync');
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+var sass = require("gulp-sass")(require("sass"));
+var uglify = require("gulp-uglifycss");
+var browserSync = require("browser-sync").create();
+var strip = require("gulp-strip-comments");
 
-gulp.task('sass', function() {
+gulp.task("sass", function () {
     return gulp
-        .src('./scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(rename({ suffix: '.min'}))
-        .pipe(gulp.dest('../dist/css'))
+        .src("./scss/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(sass({ outputStyle: "compressed" }))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("../dist/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('js', function() {
+gulp.task("js", function () {
     return gulp
-        .src('./js/*.js')
+        .src("./js/*.js")
         .pipe(strip())
         .pipe(uglify())
-        .pipe(rename({ suffix: '.min'}))
-        .pipe(gulp.dest('../dist/js'))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("../dist/js"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.series('sass', 'js'));
+gulp.task("default", gulp.series("sass", "js"));
 
 function watcher() {
-    gulp.watch('./scss/*.scss', gulp.series('sass'));
-    gulp.watch('./js/*.js', gulp.series('js'));
+    gulp.watch("./scss/*.scss", gulp.series("sass"));
+    gulp.watch("./js/*.js", gulp.series("js"));
 }
 
 exports.watcher = watcher;
